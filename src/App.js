af02741e2255930d5e8aaf27b98faa18f233b769
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-10-28 02:56:51 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import NotesDir from './components/NotesDir'
-import Note from './components/Note'
+import Routes from './routes'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <NotesDir />
-        <Note />
+        <Routes />
       </div>
     );
   }
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import {Switch, Route} from "react-router-dom"
+import Note from '../components/Note'
+
+const Routes = (props) => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Note} />
+      {/* <Route exact path="/videos" component={VideosPage} />
+      <Route exact path="/videos/new" component={NewVideoPage} />
+      <Route exact path="/videos/:id" component={VideoPage} />
+      <Route exact path="/users" component={UsersPage} />
+      <Route exact path="/users/:username" component={UserPage} />
+      <Route exact path="/signup" component={SignUpPage} />
+      <Route exact path="/login" component={LoginPage} /> */}
+      {/* <Route path="*" component={NoMatch} /> */}
+    </Switch>
+  )
+}
+
+export default Routes
