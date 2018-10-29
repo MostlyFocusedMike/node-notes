e8fec091d5da41a22295329b22a03c9ff1494d10
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-10-28 22:50:09 -0400

    switch router to allow for passing down props

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,18 +3,21 @@ import {Switch, Route} from "react-router-dom"
 import Note from '../components/Note'
 
 const Routes = (props) => {
+  console.log('props', props)
   return (
     <Switch>
-      <Route exact path="/" component={Note} />
-      <Route exact path="/notes/:fileName" component={Note} />
-      {/* <Route exact path="/videos" component={VideosPage} />
-      <Route exact path="/videos/new" component={NewVideoPage} />
-      <Route exact path="/videos/:id" component={VideoPage} />
-      <Route exact path="/users" component={UsersPage} />
-      <Route exact path="/users/:username" component={UserPage} />
-      <Route exact path="/signup" component={SignUpPage} />
-      <Route exact path="/login" component={LoginPage} /> */}
-      <Route path="*" component={Note} />
+      <Route 
+        exact path="/" 
+        render={(props) => <Note {...props} />}
+      />
+      <Route 
+        exact path="/notes/:fileName" 
+        render={(props) => <Note {...props} />}
+      />
+      <Route 
+        exact path="*" 
+        render={(props) => <Note {...props} />}
+      />
     </Switch>
   )
 }
