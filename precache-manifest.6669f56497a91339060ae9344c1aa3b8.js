99b07c988382ee7d09b6c95112175c30b0d59f12
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-10-30 00:54:12 -0400

    Updates

---
+++ precache-manifest.6669f56497a91339060ae9344c1aa3b8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "011d311bc3c155f78a97f8e49405ca9a",
+    "url": "/node-notes/static/media/oh man.011d311b.md"
+  },
+  {
+    "revision": "0a75a0674b27daa89b4d",
+    "url": "/node-notes/static/css/main.ae39a099.chunk.css"
+  },
+  {
+    "revision": "5efb3533893a26d72499",
+    "url": "/node-notes/static/js/1.5efb3533.chunk.js"
+  },
+  {
+    "revision": "27495ca058a3f935d8e7",
+    "url": "/node-notes/static/js/runtime~main.27495ca0.js"
+  },
+  {
+    "revision": "ce8bfa584c536cd39742b8e61ca41b65",
+    "url": "/node-notes/static/media/OK.ce8bfa58.md"
+  },
+  {
+    "revision": "3ac546cdb4da79fc570e396b0cad5076",
+    "url": "/node-notes/static/media/WHAT.3ac546cd.md"
+  },
+  {
+    "revision": "24429efb551c4539f31fd2e502ca3d17",
+    "url": "/node-notes/static/media/new-boy.24429efb.md"
+  },
+  {
+    "revision": "7367aa21b501a2e9e79dfc77e228547a",
+    "url": "/node-notes/static/media/new-file.7367aa21.md"
+  },
+  {
+    "revision": "3265f434b5acb3df53ac73cff41905b2",
+    "url": "/node-notes/static/media/new-redirect.3265f434.md"
+  },
+  {
+    "revision": "55e3cb92f7296d9ece2f99c1ea92974d",
+    "url": "/node-notes/static/media/newer-redirect.55e3cb92.md"
+  },
+  {
+    "revision": "0a75a0674b27daa89b4d",
+    "url": "/node-notes/static/js/main.0a75a067.chunk.js"
+  },
+  {
+    "revision": "b805d3efa1cf26ccb2ca065c65132e8e",
+    "url": "/node-notes/static/media/ohhhhhh.b805d3ef.md"
+  },
+  {
+    "revision": "d812c3d4e31e4f66625fb618f94d51b2",
+    "url": "/node-notes/static/media/ok neato.d812c3d4.md"
+  },
+  {
+    "revision": "cb5baf3d6e518488e3ca4ce5c92e79ff",
+    "url": "/node-notes/static/media/ok then .cb5baf3d.md"
+  },
+  {
+    "revision": "33eefe5816b8db5e9841d576f41641df",
+    "url": "/node-notes/static/media/sections-test.33eefe58.md"
+  },
+  {
+    "revision": "994ac91aad3fbaa8b4c24e5966ab3260",
+    "url": "/node-notes/static/media/test.994ac91a.md"
+  },
+  {
+    "revision": "909ba8264932001f5f32826d1087877d",
+    "url": "/node-notes/static/media/tester.909ba826.md"
+  },
+  {
+    "revision": "b0b37e9acf1e788374938955fbb1f0fe",
+    "url": "/node-notes/static/media/testerino.b0b37e9a.md"
+  },
+  {
+    "revision": "6af8fae87fe304e839f18450964ca587",
+    "url": "/node-notes/static/media/why-refresh.6af8fae8.md"
+  },
+  {
+    "revision": "f783a1f7a75f23648bad90f5112ede10",
+    "url": "/node-notes/static/media/wowow.f783a1f7.md"
+  },
+  {
+    "revision": "5806540774634a3bca1220af55aa5cc7",
+    "url": "/node-notes/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/node-notes/precache-manifest.da5fdcdbe85d829ced893dca9dfe03a4.js"
+  "/node-notes/precache-manifest.6669f56497a91339060ae9344c1aa3b8.js"
 );
 
 workbox.clientsClaim();
