76c2a392b42ca9aa44cca399203da31e2f918a7a
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-10-30 00:06:28 -0400

    get table of contents to load properly

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -10,9 +10,37 @@ class TableOfContents extends React.Component {
     this.state = this.initState
   }
 
+  loadContents() {
+    if (this.props.text) {
+      let sections = marked(this.props.text).match(/<h1.+>.+</g).map(text => {
+        let match = text.match(/<h1 id="(.+)">(.+)</)
+        return {
+          link: match[1],
+          text: match[2]
+        }
+      })
+      this.setState({ sections })
+    }
+  }
+
+  componentDidMount() {
+    this.loadContents()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.text !== this.props.text) {
+      this.loadContents()
+    }
+  }
   render() {
     return (
-      <div> Table of contents</div>
+      <div id="table-of-contents"> 
+        <p>Table of contents</p>
+        {
+          this.state.sections.map(section => <a href={`#${section.link}`}>{section.text}</a>)
+        }
+      </div>
+
     )
   }
 }
