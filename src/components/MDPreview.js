3120a31a56f805e65850d0c83454f25574087f3d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-01-13 22:37:53 -0500

    add should component update to save on load times

--- src/components/MDPreview.js
+++ src/components/MDPreview.js
@@ -2,20 +2,21 @@ import React from 'react';
 import createMarkdown from '../helpers/createMarkdown';
 
 class MDPreview extends React.Component {
-  renderWelcome() {
-    return "# hello there \n ## start adding markdown to get started"
-  }
 
-  render() {
-    const { title, text } = this.props.note;
-    const markdown = createMarkdown(text, this.props.viewInfo.editing);
-    return (
-      <div id="md-preview">
-        <h1>Preview of: {title}</h1>
-        <div dangerouslySetInnerHTML={{__html: markdown}}></div>
-      </div>
-    )
-  }
+    shouldComponentUpdate(prevProps, prevState) {
+        return this.props.viewInfo.editing || prevProps.viewInfo.editing;
+    }
+    render() {
+        console.log('I render', );
+        const { title, text } = this.props.note;
+        const markdown = createMarkdown(text, this.props.viewInfo.editing);
+        return (
+        <div id="md-preview">
+            <h1>Preview of: {title}</h1>
+            <div dangerouslySetInnerHTML={{__html: markdown}}></div>
+        </div>
+        )
+    }
 }
 
 export default MDPreview
